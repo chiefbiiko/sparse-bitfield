7eb36bcde56329742d5907a282ec7cd239fe6447
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-06-06 22:30:11 +0200

    fix import

--- mod.ts
+++ mod.ts
@@ -1,4 +1,7 @@
-import { Pager, Page } from "./../memory-pager/mod.ts";
+import {
+  Pager,
+  Page
+} from "https://denopkg.com/chiefbiiko/memory-pager/mod.ts";
 
 /** Is the given number a power of two? */
 function powerOfTwo(x: number): boolean {
@@ -19,10 +22,10 @@ export class Bitfield {
   readonly pageOffset: number;
   readonly pageSize: number;
   readonly pages: Pager;
-  
+
   byteLength: number;
   length: number;
-  
+
   private _trackUpdates: boolean;
   private _pageMask: number;
 
@@ -31,21 +34,21 @@ export class Bitfield {
     if (opts instanceof Uint8Array) {
       opts = { buffer: opts };
     }
-    
+
     this.pageOffset = opts.pageOffset || 0;
     this.pageSize = opts.pageSize || 1024;
     this.pages = opts.pages || new Pager(this.pageSize);
-    
+
     this.byteLength = this.pages.length * this.pageSize;
     this.length = 8 * this.byteLength;
-    
+
     if (!powerOfTwo(this.pageSize)) {
       throw new Error("The page size should be a power of two");
     }
-    
+
     this._trackUpdates = !!opts.trackUpdates;
     this._pageMask = this.pageSize - 1;
-    
+
     if (opts.buffer) {
       for (let i: number = 0; i < opts.buffer.length; i += this.pageSize) {
         this.pages.set(
@@ -53,7 +56,7 @@ export class Bitfield {
           opts.buffer.slice(i, i + this.pageSize)
         );
       }
-      
+
       this.byteLength = opts.buffer.length;
       this.length = 8 * this.byteLength;
     }
@@ -64,7 +67,7 @@ export class Bitfield {
     const o: number = i & this._pageMask;
     const j: number = (i - o) / this.pageSize;
     const page: Page = this.pages.get(j, true);
-    
+
     return page ? page.buffer[o + this.pageOffset] : 0;
   }
 
@@ -77,18 +80,18 @@ export class Bitfield {
     if (page.buffer[o] === b) {
       return false;
     }
-    
+
     page.buffer[o] = b;
-    
+
     if (i >= this.byteLength) {
       this.byteLength = i + 1;
       this.length = this.byteLength * 8;
     }
-    
+
     if (this._trackUpdates) {
       this.pages.updated(page);
     }
-    
+
     return true;
   }
 
@@ -96,7 +99,7 @@ export class Bitfield {
   get(i: number): boolean {
     const o: number = i & 7;
     const j: number = (i - o) / 8;
-    
+
     return !!(this.getByte(j) & (128 >> o));
   }
 
@@ -105,17 +108,17 @@ export class Bitfield {
     const o: number = i & 7;
     const j: number = (i - o) / 8;
     const b: number = this.getByte(j);
-    
+
     return this.setByte(j, v ? b | (128 >> o) : b & (255 ^ (128 >> o)));
   }
 
   /** Gets a single buffer representing the entire bitfield. */
   toBuffer(): Uint8Array {
     const all: Uint8Array = new Uint8Array(this.pages.length * this.pageSize);
-    
+
     for (let i: number = 0; i < this.pages.length; i++) {
       const next: Page = this.pages.get(i, true);
-      
+
       if (next) {
         all
           .subarray(i * this.pageSize)
@@ -127,7 +130,7 @@ export class Bitfield {
           );
       }
     }
-    
+
     return all;
   }
 }
