982ddfa838bea33629fd576c1763fa5d000cec54
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-07-16 20:29:51 +0200

    fix testing imports

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
-import { test, runIfMain } from "https://deno.land/x/testing/mod.ts";
-import { assertEquals } from "https://deno.land/x/testing/asserts.ts";
+import { test, runIfMain } from "https://deno.land/std/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Bitfield } from "./mod.ts";
 
 /** Concatenates given buffers. */
